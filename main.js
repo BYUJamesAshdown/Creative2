8eb53a91d00e7efb166bfeafaee9f71b77308087
James A <james.ashdown@byu.net>
2018-10-03 22:57:35 +0000

    Added jQuery Get request for Hearthstone data

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 /* global $ */
 $(document).ready(function() {
+    var url = "https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json";
     
-})
+    $.get(url).done(function(data) {
+        console.log(data);
+    });
+});
